aa6217036c84f17c227cbc2cbb60f112e81802e9
SMiller <smiller77@email.arizona.edu>
2017-09-06 14:30:28 -0700

    More experiments

--- src/atsd_experiment.m
+++ src/atsd_experiment.m
@@ -24,33 +24,43 @@ function [] = atsd_experiment(datasets, params)
                 [datatr, datate] = splitData(n, data, params.split);
 
                 tic;
-                x = anti_training(datatr, ftype, params);
+                pareto = anti_training(datatr, ftype, params);
                 timers(i, ftype) = timers(i, ftype) + toc;
 
-                err_best = intmax;
+				disp(['    Pareto front size: ', size(pareto, 1)])
 
-                for j = 1:size(x, 1)
+				err_all = 0;
+                err_best = intmax;
+				err_best3 = [intmax intmax intmax];
+
+                for j = 1:size(pareto, 1)
                     % run desired classifier
                     model = blackbox(datatr, ...
                         'classifier', classifier, ...
-                        'freeparams', x(j, :), ...
+                        'freeparams', pareto(j, :), ...
                         'dokfold', false);
 
                     yhat = predict(model, datate(:, 1:end-1));
                     err = calcError(yhat, datate(:, end));
 
-                    if err < err_best
-                        err_best = err;
-                        stats = calcStats(datate(:, end), yhat);
-                        fms_best = mean(stats.fscore);
-                    end
+					[err_max,idx] = max(err_best3)
+					if err < err_max:
+						err_best3(idx) = err;
+					end
+
+                    err_best = min(err_best3);
+					err_all = err_all + err;
                 end
-
-                all_fms_moo(i, ftype) = all_fms_moo(i, ftype) + fms_best;
-                errors(i, ftype) = errors(i, ftype) + err_best;
-
+                %all_fms_moo(i, ftype) = all_fms_moo(i, ftype) + fms_best;
+				errors_all(i, ftype) = errors_all(i,ftype) + ...
+											err_all/size(pareto,1);
+                errors_best(i, ftype) = errors_best(i, ftype) + err_best;
+				errors_best_3(i, ftype) = errors_best_3(i, ftype) + ...
+											mean(err_best3);
 				% save averages
-				results.atsd_errors = errors./n;
+				results.atsd_errors_all = errors_all./n
+				results.atsd_errors_best = errors_best./n;
+				results.atsd_errors_best_3 = errors_best_3./n;
 				results.atsd_timers = timers./n;
                 save(['outputs/raw_outputs/', classifier, ...
 							'_atsd_results.mat'], results);
@@ -58,8 +68,6 @@ function [] = atsd_experiment(datasets, params)
         end
     end
     
-	%save averages
-    results.atsd_errors = errors./numRuns;
-    results.atsd_timers = timers./numRuns;
+	%save results
     save(['outputs/raw_outputs/', classifier, '_atsd_results.mat'], results);
 end
